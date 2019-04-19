9451a6ad63817e9f1374c36e9269e64d33aefc66
Marc Bernini <bernini.marc@gmail.com>
2019-04-19 18:06:32 +0200

    quest angular 06 in progress. Training before challenge

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EmailValidator } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
+import { Order } from 'src/app/class/order';
 
 @Component({
   selector: 'app-sign-up',
@@ -10,15 +11,20 @@ import { FormsModule } from '@angular/forms';
 export class SignUpComponent implements OnInit {
 
   email = '';
-
-  constructor() {}
-
+  model: Order = new Order();
   hover = true;
   showMovies = false;
+  constructor() {}
 
   ngOnInit() {}
 
   displayMoviesList() {
     this.showMovies = !this.showMovies;
   }
+
+  onSubmit() {
+    // form submitted
+    console.log(this.model);
+  }
+
 }
